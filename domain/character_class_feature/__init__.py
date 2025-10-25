90e814f6858287f158ee48da9ae43cc68ca66808
Vladimir German <vladimir.german.and@yandex.ru>
2025-10-25 21:35:20 +0700

    change class featur aggregate

--- domain/character_class_feature/__init__.py
+++ domain/character_class_feature/__init__.py
@@ -1,13 +1,3 @@
-from .description import ClassFeatureDescription
 from .feature import ClassFeature
-from .feature_id import ClassFeatureID
-from .level import ClassFeatureLevel
-from .name import ClassFeatureName
 
-__all__ = [
-    "ClassFeatureDescription",
-    "ClassFeature",
-    "ClassFeatureID",
-    "ClassFeatureLevel",
-    "ClassFeatureName",
-]
+__all__ = ["ClassFeature"]
--- domain/character_class_feature/feature.py
+++ domain/character_class_feature/feature.py
@@ -1,48 +1,83 @@
-from domain.character_class import ClassID
-from domain.character_class_feature.description import ClassFeatureDescription
-from domain.character_class_feature.feature_id import ClassFeatureID
-from domain.character_class_feature.level import ClassFeatureLevel
-from domain.character_class_feature.name import ClassFeatureName
+from uuid import UUID
+
+from domain.error import DomainError
 
 
 class ClassFeature:
     def __init__(
         self,
-        feature_id: ClassFeatureID,
-        class_id: ClassID,
-        name: ClassFeatureName,
-        level: ClassFeatureLevel,
-        description: ClassFeatureDescription,
+        feature_id: UUID,
+        class_id: UUID,
+        name: str,
+        description: str,
+        level: int,
     ) -> None:
+        self.__validate_description(description)
+        self.__validate_name(name)
+        self.__validate_level(level)
         self.__feature_id = feature_id
         self.__class_id = class_id
         self.__name = name
-        self.__level = level
         self.__description = description
+        self.__level = level
 
-    def feature_id(self) -> ClassFeatureID:
+    def feature_id(self) -> UUID:
         return self.__feature_id
 
-    def class_id(self) -> ClassID:
+    def class_id(self) -> UUID:
         return self.__class_id
 
-    def name(self) -> ClassFeatureName:
+    def name(self) -> str:
         return self.__name
 
-    def level(self) -> ClassFeatureLevel:
-        return self.__level
-
-    def description(self) -> ClassFeatureDescription:
+    def description(self) -> str:
         return self.__description
 
-    def new_name(self, name: ClassFeatureName) -> None:
+    def level(self) -> int:
+        return self.__level
+
+    def new_name(self, name: str) -> None:
+        if self.__name == name:
+            raise DomainError.idempotent(
+                "текущее название умения равно новому названию умения"
+            )
+        self.__validate_name(name)
         self.__name = name
 
-    def new_level(self, level: ClassFeatureLevel) -> None:
-        self.__level = level
-
-    def new_description(self, description: ClassFeatureDescription) -> None:
+    def new_description(self, description: str) -> None:
+        self.__validate_description(description)
         self.__description = description
 
+    def new_level(self, level: int) -> None:
+        if self.__level == level:
+            raise DomainError.idempotent(
+                "текущее уровень умения равен новому уровень умения"
+            )
+        self.__validate_level(level)
+        self.__level = level
+
+    def __validate_name(self, name: str) -> None:
+        if len(name) == 0:
+            raise DomainError.invalid_data("название умения не может быть пустым")
+        if len(name) > 50:
+            raise DomainError.invalid_data(
+                "название умения не может превышать длину в 50 символов"
+            )
+
+    def __validate_description(self, description: str) -> None:
+        if len(description) == 0:
+            raise DomainError.invalid_data("описание умения не может быть пустым")
+
+    def __validate_level(self, level: int) -> None:
+        if level < 1 or level > 20:
+            raise DomainError.invalid_data(
+                "уровень умения должен находиться в диапазоне от 1 до 20"
+            )
+
     def __str__(self) -> str:
-        return f"название: {self.__name}"
+        return self.__name
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, self.__class__):
+            return self.__feature_id == value.__feature_id
+        raise NotImplemented
