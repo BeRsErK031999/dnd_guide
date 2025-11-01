ddf387fbeb8a8deabf0ffde6433d289afff62113
Vladimir German <vladimir.german.and@yandex.ru>
2025-11-01 20:12:37 +0700

    change class feature repository and service

--- domain/class_feature/repository.py
+++ domain/class_feature/repository.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+from uuid import UUID
 
 
 class ClassFeatureRepository(ABC):
     @abstractmethod
-    async def is_name_exist(self, name: str) -> bool:
+    async def is_name_for_class_exist(self, class_id: UUID, name: str) -> bool:
         raise NotImplemented
--- domain/class_feature/service.py
+++ domain/class_feature/service.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from domain.class_feature.repository import ClassFeatureRepository
 
 
@@ -5,8 +7,8 @@ class ClassFeatureService:
     def __init__(self, class_feature_repository: ClassFeatureRepository) -> None:
         self.__repository = class_feature_repository
 
-    async def can_create_with_name(self, name: str) -> bool:
-        return await self.__repository.is_name_exist(name)
+    async def can_create_for_class_with_name(self, class_id: UUID, name: str) -> bool:
+        return await self.__repository.is_name_for_class_exist(class_id, name)
 
-    async def can_rename_with_name(self, name: str) -> bool:
-        return await self.__repository.is_name_exist(name)
+    async def can_rename_for_class_with_name(self, class_id: UUID, name: str) -> bool:
+        return await self.__repository.is_name_for_class_exist(class_id, name)
