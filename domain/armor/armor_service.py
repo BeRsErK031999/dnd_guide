9a405dac6b6a56c3cfdae5a12d5d3deb8eca434d
Vladimir German <vladimir.german.and@yandex.ru>
2025-11-01 19:25:30 +0700

    create armor repository and service

---
+++ domain/armor/armor_service.py
@@ -0,0 +1,12 @@
+from domain.armor.armor_repository import ArmorRepository
+
+
+class ArmorService:
+    def __init__(self, armor_repository: ArmorRepository) -> None:
+        self.__repository = armor_repository
+
+    async def can_create_with_name(self, name: str) -> bool:
+        return await self.__repository.is_name_exist(name)
+
+    async def can_rename_with_name(self, name: str) -> bool:
+        return await self.__repository.is_name_exist(name)
