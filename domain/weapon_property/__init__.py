8b8f0ae9dc5384eadff8ca273bb1650d94c9d96e
Vladimir German <vladimir.german.and@yandex.ru>
2025-11-02 11:13:03 +0700

    create weapon property repository and service in domain layer

--- domain/weapon_property/__init__.py
+++ domain/weapon_property/__init__.py
@@ -1,4 +1,11 @@
 from .name import WeaponPropertyName
+from .repository import WeaponPropertyRepository
+from .service import WeaponPropertyService
 from .weapon_property import WeaponProperty
 
-__all__ = ["WeaponPropertyName", "WeaponProperty"]
+__all__ = [
+    "WeaponPropertyName",
+    "WeaponPropertyRepository",
+    "WeaponPropertyService",
+    "WeaponProperty",
+]
---
+++ domain/weapon_property/service.py
@@ -0,0 +1,12 @@
+from domain.weapon_property.repository import WeaponPropertyRepository
+
+
+class WeaponPropertyService:
+    def __init__(self, weapon_kind_repository: WeaponPropertyRepository) -> None:
+        self.__repository = weapon_kind_repository
+
+    async def can_create_with_name(self, name: str) -> bool:
+        return await self.__repository.is_name_exist(name)
+
+    async def can_rename_with_name(self, name: str) -> bool:
+        return await self.__repository.is_name_exist(name)
