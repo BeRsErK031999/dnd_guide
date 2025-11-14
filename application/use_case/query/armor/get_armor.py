65615afce810dea7949d92248625fa83e5d1eb98
Vladimir German <vladimir.german.and@yandex.ru>
2025-11-14 18:13:38 +0700

    create armor query use case

--- application/use_case/query/armor/get_armor.py
+++ application/use_case/query/armor/get_armor.py
@@ -1,6 +1,7 @@
 from application.dto.query.armor import ArmorQuery
 from application.repository import ArmorRepository
 from domain.armor import Armor
+from domain.error import DomainError
 
 
 class GetArmorUseCase:
@@ -8,4 +9,6 @@ class GetArmorUseCase:
         self.__repository = repository
 
     async def execute(self, query: ArmorQuery) -> Armor:
+        if not await self.__repository.id_exists(query.armor_id):
+            raise DomainError.not_found(f"доспехов с id {query.armor_id} не существует")
         return await self.__repository.get_by_id(query.armor_id)
